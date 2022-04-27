97474fc4b2b046ece78a95459b6d35d6e9781155
andrescuchi <104508454+andrescuchi@users.noreply.github.com>
2022-04-27 11:04:07 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,74 @@
+while (true) {
+    servos.P0.setAngle(0)
+    basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # # #
+    . . . . .
+    . . . . .
+    `)
+    basic.pause(1000)
+    servos.P0.setAngle(45)
+    basic.showLeds(`
+    . . . . #
+    . . . # .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+    basic.pause(1000)
+    servos.P0.setAngle(90)
+    basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+    basic.pause(1000)
+    servos.P0.setAngle(135)
+    basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+    basic.pause(1000)
+    servos.P0.setAngle(180)
+    basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # # . .
+    . . . . .
+    . . . . .
+    `)
+    basic.pause(1000)
+    servos.P0.setAngle(135)
+    basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+    basic.pause(1000)
+    servos.P0.setAngle(90)
+    basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+    basic.pause(1000)
+    servos.P0.setAngle(45)
+    basic.showLeds(`
+    . . . . #
+    . . . # .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+    basic.pause(1000)
+}
